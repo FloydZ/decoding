2b02358be20f5ace00a0f4c4963198dbe1cfa26c
Floyd Zweydinger <floyd.zweydinger@rub.de>
2022-03-15 16:40:28 +0400

    removed fplll from buils script

--- setup.sh
+++ setup.sh
@@ -13,9 +13,9 @@ git apply patch.diff
 cd ../../
 cd deps/fplll
 git checkout 5.3.0
-./autogen.sh
-./configure
-make -j8
+#./autogen.sh
+#./configure
+#make -j8
 cd ../../
 cd ../../
 mkdir cmake-build-release && cd cmake-build-release && cmake -DCMAKE_BUILD_TYPE=Release .. && cd ..
